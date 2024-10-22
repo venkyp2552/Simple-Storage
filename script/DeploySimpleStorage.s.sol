920d75f21b23404ddfb946f690da1fea11d121a9
root <root@DESKTOP-1HVSU4I>
2024-10-22 17:02:24 +0530

    Finally Completed

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.18;
 
 import {Script} from "../lib/forge-std/src/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
+
 contract DeploySimpleStorage is Script {
-    function run() external returns(SimpleStorage) {
+    function run() external returns (SimpleStorage) {
         vm.startBroadcast();
         SimpleStorage simpleStorage = new SimpleStorage();
         vm.stopBroadcast();
         return simpleStorage;
-        
     }
 }
--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -1,27 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract SimpleStorage{
+contract SimpleStorage {
     uint256 myFavoriteNumber;
 
-    struct Person{
+    struct Person {
         uint256 favNumber;
         string name;
     }
 
-    mapping(string=>uint256) public nameToFavoriteNumber;
+    mapping(string => uint256) public nameToFavoriteNumber;
 
     Person[] public listOfPeople;
 
     function store(uint256 _favroiteNumber) public {
-        myFavoriteNumber=_favroiteNumber;
+        myFavoriteNumber = _favroiteNumber;
     }
-    function retrive() public view returns(uint256){
+
+    function retrive() public view returns (uint256) {
         return myFavoriteNumber;
     }
 
-    function addPerson(string memory _name,uint256 _fanNum) public {
-        listOfPeople.push(Person(_fanNum,_name));
-        nameToFavoriteNumber[_name]=_fanNum;
+    function addPerson(string memory _name, uint256 _fanNum) public {
+        listOfPeople.push(Person(_fanNum, _name));
+        nameToFavoriteNumber[_name] = _fanNum;
     }
 }
